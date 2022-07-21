2bed11d4bba382c0c2b8a7e13faa038cfb9badaf
JamesWJager <100589762+JamesWJager@users.noreply.github.com>
2022-07-21 18:09:35 -0400

    Initial commit

---
+++ gatsby-config.ts
@@ -0,0 +1,34 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Gatsby CMS blueprint`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "",
+      "spaceId": ""
+    }
+  }, "gatsby-plugin-postcss", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "GA TODO"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
+
+export default config;
